d0098e18e54e58058beb2b3d903398d751124257
Sprinuts <72307963+Sprinuts@users.noreply.github.com>
2025-06-13 12:48:17 +0800

    try fix for css not loading in deployment

--- MentalEase/resources/views/welcome.blade.php
+++ MentalEase/resources/views/welcome.blade.php
@@ -8,8 +8,8 @@
     <!-- Stylesheets -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('style/landingnavbar.css') }}">
-    <link rel="stylesheet" href="{{ asset('style/welcomepage.css') }}">
+    <link rel="stylesheet" href="{{ asset('style/landingnavbar.css', true) }}">
+    <link rel="stylesheet" href="{{ asset('style/welcomepage.css', true) }}">
 </head>
 <body>
 
