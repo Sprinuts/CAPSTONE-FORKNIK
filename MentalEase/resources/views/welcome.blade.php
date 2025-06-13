896b06533a0f8edf7145ab318cbbeba4af038bbe
Sprinuts <72307963+Sprinuts@users.noreply.github.com>
2025-06-13 14:31:02 +0800

    rollback http not secure, di nagana css

--- MentalEase/resources/views/welcome.blade.php
+++ MentalEase/resources/views/welcome.blade.php
@@ -8,10 +8,12 @@
     <!-- Stylesheets -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('style/landingnavbar.css', true) }}">
-    <link rel="stylesheet" href="{{ asset('style/welcomepage.css', true) }}">
+    <link rel="stylesheet" href="{{ asset('style/landingnavbar.css') }}">
+    <link rel="stylesheet" href="{{ asset('style/welcomepage.css') }}">
 </head>
 <body>
+
+@include('include.landingnavbar')
 
 <!-- Hero Section (Home) -->
 <section id="home" class="hero parallax-section" style="background-image: url('{{ asset('style/assets/parallax-bg1.jpg') }}'); background-color: #F4F4F2;">
