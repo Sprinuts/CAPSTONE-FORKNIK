c3719bb757fd131a8976ba6ab05aa7ed9d0d305e
Sprinuts <72307963+Sprinuts@users.noreply.github.com>
2025-06-25 00:55:30 +0800

    try fix mail

--- MentalEase/routes/web.php
+++ MentalEase/routes/web.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('maintenance');
 })->name('welcome');
 
 Route::get('/welcome', function () {
